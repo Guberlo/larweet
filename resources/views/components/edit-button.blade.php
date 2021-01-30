82393c3b28a58259f64b72173b4fb3395f2645b2
Guberlo <salasero24@gmail.com>
2021-01-30 20:26:17 +0000

    refactor: move edit button to separate component

--- resources/views/components/edit-button.blade.php
+++ resources/views/components/edit-button.blade.php
@@ -1,3 +1,4 @@
-@unless (auth()->user()->isNot($user))
-    <a href="" class="bg-gray-100 rounded-full border border-gray-300 py-2 px-4 text-black-200 text-sm mr-2">Edit Profile</a>
-@endunless
+@can('edit', $user)
+    <a href="/users/{{$user->name}}/edit" class="bg-gray-100 rounded-full border border-gray-300 py-2 px-4 text-black-200 text-sm mr-2">Edit Profile</a>
+@endcan
+
