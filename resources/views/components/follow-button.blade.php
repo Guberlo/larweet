e2e6f79b9fb8572ecbd3b5544315ba042a1de8ae
Guberlo <salasero24@gmail.com>
2021-01-30 20:08:34 +0000

    fix: follow button visible in logged user page

--- resources/views/components/follow-button.blade.php
+++ resources/views/components/follow-button.blade.php
@@ -1,6 +1,8 @@
+@unless (auth()->user()->is($user))
 <form action="/users/follow/{{ $user->name }}" method="post">
     @csrf
     <button type="submit" href="" class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-sm">
         {{ auth()->user()->isFollowing($user) ? 'Unfollow Me' : 'Follow Me' }}
     </button>
 </form>
+@endunless
